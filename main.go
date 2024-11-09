04e4b1ad40f1fe35a7e2392fc9d095f134e7c5f2
aaitbihi <aymanaitbihi9@gmail.com>
2024-11-09 15:31:48 +0100

    trying ant mouvements

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ type Room struct {
 	IsStart bool
 	IsEnd   bool
 }
+type Ant struct {
+	Current string
+	Num     int
+	path    []string
+}
 
 var (
 	seencor = make(map[string]bool)
@@ -59,6 +64,29 @@ func main() {
 	for _, path := range paths {
 		fmt.Println(path)
 	}
+
+	Antmovement(numAnts, start, end, paths)
+}
+
+func Antmovement(ants int, start, end string, paths [][]string) {
+	Ants := make([]Ant, ants)
+
+	for i := 1; i <= ants; i++ {
+		Ants[i-1].Num = i
+		Ants[i-1].path = paths[i%len(paths)]
+		Ants[i-1].Current = Ants[i-1].path[0]
+	}
+	
+	moves := []string{}
+	
+	for _, ant := range Ants {
+		for _, p := range ant.path[1:] {
+			num := ant.Num
+			moves = append(moves, fmt.Sprintf("L%d-%v", num, p))
+		}
+	}
+	fmt.Println(Ants)
+	fmt.Println(moves)
 }
 
 func parseInput(filename string) (int, map[string]*Room, map[string][]string, error) {
