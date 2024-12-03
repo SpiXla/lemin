e290a4f21da5180444573479f2f74b30fad79e37
Your Name <you@example.com>
2024-12-03 12:01:54 +0100

    links

--- Input/Tunnels.go
+++ Input/Tunnels.go
@@ -15,6 +15,10 @@ func parseTunnel(line string, connections map[string][]string) error {
 	if len(parts) != 2 || parts[0] == parts[1] {
 		return errors.New("invalid data format, invalid links")
 	}
+	if !seename[parts[0]] || !seename[parts[1]] {
+		return errors.New("invalid data format, invalid links")
+
+	}
 
 	connections[parts[0]] = append(connections[parts[0]], parts[1])
 	connections[parts[1]] = append(connections[parts[1]], parts[0])
