5d73f1f448c75c15bd0135a1edb6c73d2484aad4
El-gharras Kerzazi <ekerzazi@noreply.learn.zone01oujda.ma>
2024-12-03 12:51:28 +0000

    dfs

--- functions/FindPaths.go
+++ functions/FindPaths.go
@@ -7,7 +7,6 @@ import (
 )
 
 func FindUniquePaths(rooms map[string]input.Room, connections map[string][]string) ([][]string, [][]string, error) {
-
 	start, end := FindStartEnd(rooms)
 
 	if start == "" || end == "" {
@@ -16,27 +15,27 @@ func FindUniquePaths(rooms map[string]input.Room, connections map[string][]strin
 
 	var paths [][]string
 	visited := map[string]bool{}
-	var dfs func(path []string)
 
-	dfs = func(path []string) {
-		room := path[len(path)-1]
-		if room == end {
-			paths = append(paths, append([]string{}, path...))
-			return
-		}
-		visited[room] = true
-		for _, neighbor := range connections[room] {
-			if !visited[neighbor] {
-				dfs(append(path, neighbor))
-			}
-		}
-		visited[room] = false
-	}
-	dfs([]string{start})
+	dfs([]string{start}, start, end, &paths, visited, connections)
 	if len(paths) == 0 {
 		return nil, nil, errors.New("invalid data format, no path found")
 	}
 	return filterUniquePaths(paths, true), filterUniquePaths(paths, false), nil
+}
+
+func dfs(path []string, start, end string, paths *[][]string, visited map[string]bool, connections map[string][]string) {
+	room := path[len(path)-1]
+	if room == end {
+		*paths = append(*paths, append([]string{}, path...))
+		return
+	}
+	visited[room] = true
+	for _, neighbor := range connections[room] {
+		if !visited[neighbor] {
+			dfs(append(path, neighbor), start, end, paths, visited, connections)
+		}
+	}
+	visited[room] = false
 }
 
 func FindStartEnd(rooms map[string]input.Room) (string, string) {
