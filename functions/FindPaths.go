026c3f704debcaed37618e9739e67e1fcbc8edb9
ayman Ait Bihi <aaitbihi@noreply.learn.zone01oujda.ma>
2024-12-05 14:10:23 +0000

    Update 'functions/FindPaths.go'

--- functions/FindPaths.go
+++ functions/FindPaths.go
@@ -1,64 +1,155 @@
 package functions
 
 import (
-	"errors"
+    "errors"
+    "fmt"
 
-	input "lemin/Input"
+    input "lemin/Input"
 )
 
 func GetNumberOfConnections(connections map[string][]string) int {
-	var Counter int
-	for _, value := range connections {
-		Counter += len(value)
-	}
-	return Counter
+    var Counter int
+    for _, value := range connections {
+        Counter += len(value)
+    }
+    return Counter
 }
 
-func FindUniquePaths(rooms map[string]input.Room, connections map[string][]string) ([][]string, [][]string, error) {
-	start, end := FindStartEnd(rooms)
+func FindUniquePaths(rooms map[string]input.Room, connections map[string][]string) ([][]string, error) {
+    start, end := FindStartEnd(rooms)
+    var paths [][]string
+    visited := map[string]bool{}
 
-	if start == "" || end == "" {
-		return nil, nil, errors.New("start or end room not defined")
-	}
-	var paths [][]string
-	visited := map[string]bool{}
+    if start == "" || end == "" {
+        return nil, errors.New("start or end room not defined")
+    }
 
-	if Num := GetNumberOfConnections(connections); Num > 200 {
-		return nil, nil, errors.New("unsupported graph")
-	}
+    if Num := GetNumberOfConnections(connections); Num > 200 {
+        return nil, errors.New("unsupported graph")
+    }
 
-	DepthFirstSearch([]string{start}, start, end, &paths, visited, connections)
-	if len(paths) == 0 {
-		return nil, nil, errors.New("invalid data format, no path found")
-	}
+    shortestPath := [][]string{}
 
-	return filterUniquePaths(paths, true), filterUniquePaths(paths, false), nil
+    // DepthFirstSearch([]string{start}, start, end, &paths, visited, connections)
+    ConnWithoutStart := RemoveStart(&paths, connections, start)
+    fmt.Println(connections[start])
+    for _, StartConnection := range connections[start] {
+        path := BFS(ConnWithoutStart, StartConnection, end)
+        shortestPath = append(shortestPath, path)
+    }
+    SortedPaths := SortPaths(shortestPath)
+    for _, path := range SortedPaths {
+        p := path[:len(path)-1]
+        for i, room := range p {
+            if !visited[room] {
+                visited[room] = true
+            } else {
+                roomBefore := p[i-1]
+                if len(connections[roomBefore]) > 2 {
+                    connections = RemoveConn(connections, roomBefore, room)
+                    path := BFS(ConnWithoutStart, p[0], end)
+                    shortestPath = append(shortestPath, path)
+
+                }
+            }
+        }
+    }
+    SortedPaths = SortPaths(shortestPath)
+    fmt.Println(SortedPaths)
+    // spaths := SortPaths(paths)
+
+    return paths, nil
 }
 
-func DepthFirstSearch(path []string, start, end string, paths *[][]string, visited map[string]bool, connections map[string][]string) {
+func RemoveConn(connections map[string][]string, roomBefore, link string) map[string][]string {
+    fmt.Println(link,roomBefore)
+    links := []string{}
+    for _, r := range connections[roomBefore] {
+        if r != link {
+            links = append(links, r)
+        }
+    }
+    connections[roomBefore] = links
+    rooms := []string{}
+    for _, r := range connections[link] {
+        if r != roomBefore {
+            rooms = append(rooms, r)
+        }
+    }
+    connections[link] = rooms
+    return connections
+}
 
-	room := path[len(path)-1]
+func BFS(graph map[string][]string, start, end string) []string {
+    queue := [][]string{{start}}
+    visited := make(map[string]bool)
+    visited[start] = true
 
-	if room == end {
-		*paths = append(*paths, append([]string{}, path...))
-	}
-	visited[room] = true
-	for _, neighbor := range connections[room] {
-		if !visited[neighbor] {
-			DepthFirstSearch(append(path, neighbor), start, end, paths, visited, connections)
-		}
-	}
-	visited[room] = false
+    for len(queue) > 0 {
+        path := queue[0]
+        queue = queue[1:]
+
+        node := path[len(path)-1]
+        if node == end {
+            return path
+        }
+        for _, neighbor := range graph[node] {
+            if !visited[neighbor] {
+                visited[neighbor] = true
+                newPath := append([]string{}, path...)
+                newPath = append(newPath, neighbor)
+                queue = append(queue, newPath)
+            }
+        }
+    }
+    return nil
+}
+
+func RemoveStart(paths *[][]string, connections map[string][]string, start string) map[string][]string {
+    for i, path := range *paths {
+        (*paths)[i] = path[1:]
+    }
+
+    for key, value := range connections {
+        if key == start {
+            delete(connections, key)
+        }
+        var newSlice []string
+
+        for _, room := range value {
+            if room != start {
+                newSlice = append(newSlice, room)
+            }
+        }
+        connections[key] = newSlice
+
+    }
+    return connections
 }
 
 func FindStartEnd(rooms map[string]input.Room) (string, string) {
-	var start, end string
-	for name, room := range rooms {
-		if room.IsStart {
-			start = name
-		} else if room.IsEnd {
-			end = name
-		}
-	}
-	return start, end
+    var start, end string
+    for name, room := range rooms {
+        if room.IsStart {
+            start = name
+        } else if room.IsEnd {
+            end = name
+        }
+    }
+    return start, end
 }
+
+// func DepthFirstSearch(path []string, start, end string, paths *[][]string, visited map[string]bool, connections map[string][]string) {
+//     room := path[len(path)-1]
+
+//     if room == end {
+//         *paths = append(*paths, append([]string{}, path...))
+//     }
+//     visited[room] = true
+//     for _, neighbor := range connections[room] {
+//         if !visited[neighbor] {
+//             DepthFirstSearch(append(path, neighbor), start, end, paths, visited, connections)
+//         }
+//     }
+//     visited[room] = false
+// }
